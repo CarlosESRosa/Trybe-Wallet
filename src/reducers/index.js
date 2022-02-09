19167c0731abdb8df4cb247cd9a4579b0c335467
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-02-09 14:44:41 -0300

    como desenvolver setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,23 @@
-// import user from './user';
-// import wallet from './wallet';
+import { combineReducers } from 'redux';
+import user from './user';
+import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const rootReducer = combineReducers({
+  user,
+  wallet,
+});
+
+export default rootReducer;
