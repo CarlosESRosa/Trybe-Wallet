a4e21d6f36de6b9241bbef952f54e3ac411241e8
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-02-09 16:06:23 -0300

    fix Route, requisito 1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
-import Login from './pages/Login';
-
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={ store }>
       <App />
-      <Route exact path="/" component={ Login } />
     </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
