192e3f37eeb3e2e23e3b8dd99fabf69a7557cefb
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-02-09 15:27:21 -0300

    setup redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,16 +5,6 @@ import wallet from './wallet';
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
-const INITIAL_STATE = {
-  user: {
-    email: '',
-  },
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
-};
-
 const rootReducer = combineReducers({
   user,
   wallet,
