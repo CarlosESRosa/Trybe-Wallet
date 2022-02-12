895367a50919ed2fea8bf890c409dc74369ee1b3
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-02-12 00:00:40 -0300

    Requisito 4 to fix

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { sendExpenses } from '../actions';
+import { sendExpenses, fetchApi } from '../actions';
 
 class Wallet extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      valorDespesa: 0,
-      descricaoDespesa: '',
-      moeda: '',
-      formaPagamento: 'Dinheiro',
-      tipoDespesa: 'Alimentação',
+      value: 0,
+      description: '',
+      currency: '',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+      totalValue: 0,
     };
   }
 
@@ -25,54 +26,72 @@ class Wallet extends React.Component {
     });
   }
 
-  addExpenses = () => {
-    const { valorDespesa, descricaoDespesa, moeda, formaPagamento,
-      tipoDespesa } = this.state;
-    const { expensesAction, expenses } = this.props;
+  totalExpenses = () => {
+    const { expenses } = this.props;
+    expenses.forEach((element) => {
+      this.setState((prevState) => ({
+        totalValue: prevState.totalValue
+          + element.exchangeRates.USD.ask * element.value,
+      }));
+    });
+  }
+
+  addExpenses = async () => {
+    const { value, description, currency, method,
+      tag } = this.state;
+    const { expensesAction, expenses, getDataApi } = this.props;
+    const dataApi = await getDataApi();
     const expensesObj = {
       id: expenses.length,
-      valorDespesa,
-      descricaoDespesa,
-      moeda,
-      formaPagamento,
-      tipoDespesa,
+      value,
+      description,
+      currency,
+      method,
+      tag,
+      exchangeRates: dataApi,
     };
     expensesAction(expensesObj);
+    this.setState({ value: 0 });
+    this.totalExpenses();
   }
 
   render() {
     const { email } = this.props;
+    const { value, totalValue } = this.state;
     return (
       <div>
         TrybeWallet
         <header>
           <h3 data-testid="email-field">{`Email: ${email}`}</h3>
-          <h3 data-testid="total-field">{`Valor: ${0}`}</h3>
+          <h3 data-testid="total-field">
+            {`Total despesas: ${parseFloat(totalValue.toFixed(2))}`}
+          </h3>
           <h3 data-testid="header-currency-field">Cambio: BRL</h3>
           <form>
             <input
               type="text"
-              name="valorDespesa"
+              name="value"
               data-testid="value-input"
+              value={ value }
               placeholder="valor da despesa"
               onChange={ this.handleChange }
             />
             <input
               type="text"
-              name="descricaoDespesa"
+              name="description"
               data-testid="description-input"
               placeholder="descrição da despesa"
               onChange={ this.handleChange }
             />
             <input
               type="text"
-              name="moeda"
+              name="currency"
               data-testid="currency-input"
-              placeholder="moeda"
+              placeholder="currency"
               onChange={ this.handleChange }
             />
             <select
-              name="formaPagamento"
+              name="method"
               data-testid="method-input"
               onChange={ this.handleChange }
             >
@@ -81,7 +100,7 @@ class Wallet extends React.Component {
               <option value="cartao de debito">Cartão de débito</option>
             </select>
             <select
-              name="tipoDespesa"
+              name="tag"
               data-testid="tag-input"
               onChange={ this.handleChange }
             >
@@ -105,6 +124,7 @@ Wallet.propTypes = {
 
 const mapDispatchToProps = (dispatch) => ({
   expensesAction: (payload) => dispatch(sendExpenses(payload)),
+  getDataApi: () => dispatch(fetchApi()),
 });
 
 const mapStateToProps = (state) => ({
